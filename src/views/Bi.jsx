3205d1ee7b55efac5fa05f798d99c24fccc7ce89
tay <131620977+Elmojerry@users.noreply.github.com>
2023-08-25 11:30:15 +0100

    navbar

--- src/views/Bi.jsx
+++ src/views/Bi.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 function BI() {
   return (
-    <div className="h-all bg-gradient-to-r from-indigo-950 to-purple-800 ">
+    <div className="h-all bg-gradient-to-r from-indigo-950 to-purple-800 mt-14">
         <h1 className='text-white font-extrabold text-4xl font-kanit p-4 snap-center'>
             Análises BI 
         </h1>
--- src/views/Intro.jsx
+++ src/views/Intro.jsx
@@ -7,12 +7,12 @@ function Intro() {
   return (
     <div className="h-all">
       <Navbar />
-        <div className='mt-20 flex flex-col gap-2 md:flex-row md:justify-items-center md:items-center md:space-x-10'>
+        <div className='mt-16 flex flex-col gap-2 md:flex-row md:justify-items-center md:items-center md:space-x-10'>
           <div className='mx-4 animate-pulse'>
             <h1 className=' font-kanit text-5xl font-extrabold text-white md:text-7xl md:mt-10'
             >Obtenha <br /> engenharia de <br /> software <br /> especializada <br /> quando precisar
             </h1>
-            <h3 className='text-white font-kanit italic text-xl font-medium mt-8'>Nós nos integramos á sua equipe, 
+            <h3 className='text-white font-kanit italic text-xl font-medium mt-2'>Nós nos integramos á sua equipe, 
             ajudamos você a descobrir o software que precisa <br /> e em seguida entregamos rápidamente.</h3>
           </div>
           <div>
