5b1629d276a200f7ac564de8148e09ccbdd3f413
tay <131620977+Elmojerry@users.noreply.github.com>
2023-08-28 11:41:55 +0100

    react bak

---
+++ src/App.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import './app.css';
+import Intro from './views/Intro';
+import Web from './views/Web';
+import Mobile from './views/Mobile';
+import Bi from './views/Bi';
+import About from './views/About';
+import Footer from './components/Footer';
+import Navbar from './components/Navbar';
+
+
+
+function App() {
+ 
+
+  return (
+   
+         <div>
+            <Intro />
+            <Bi />
+            <Web/>
+            <Mobile/>
+            <About />
+            <Footer />   
+         </div>
+           
+       
+     
+        
+   )
+    
+  
+}
+
+export default App
