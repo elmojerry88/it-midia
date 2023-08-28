86321343a0999b937b980bae3f6d426a72c94b6f
tay <131620977+Elmojerry@users.noreply.github.com>
2023-08-28 11:23:14 +0100

    index move by origin

--- pages/App.jsx
+++ pages/App.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import './app.css';
-import Intro from './views/Intro';
-import Web from './views/Web';
-import Mobile from './views/Mobile';
-import Bi from './views/Bi';
-import About from './views/About';
-import Footer from './components/Footer';
-import Navbar from './components/Navbar';
+import Intro from '../src/views/Intro';
+import Web from '../src/views/Web';
+import Mobile from '../src/views/Mobile';
+import Bi from '../src/views/Bi';
+import About from '../src/views/About';
+import Footer from '../src/components/Footer';
+import Navbar from '../src/components/Navbar';
 
 
 
---
+++ pages/sobre.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+// import { Container } from './styles';
+
+function sobre() {
+  return (
+    <div>
+        página sobre
+    </div>
+  )
+}
+
+export default sobre;
